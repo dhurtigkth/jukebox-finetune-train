f3b4f9c6dbe2b6656e0cc3b40bcd55d92a02858c
dhurtigkth <100566590+dhurtigkth@users.noreply.github.com>
2024-05-16 14:37:54 +0200

    test

--- jukebox/utils/io.py
+++ jukebox/utils/io.py
@@ -44,7 +44,7 @@ def load_audio(file, sr, offset, duration, resample=True, approx=False, time_bas
     for frame in container.decode(audio=0): # Only first audio stream
         if resample:
             frame.pts = None
-            frame = resampler.resample(frame)
+            #frame = resampler.resample(frame)
         
         #frame = np.array(frame)
         print(frame)
